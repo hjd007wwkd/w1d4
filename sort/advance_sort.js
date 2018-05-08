2d41bef6c7c30ed0b383bedc3f2d66a2360525b1
Jin Ha <hjd007wwkd@hotmail.com>
2018-05-08 22:08:56 +0000

    initial

---
+++ sort/advance_sort.js
@@ -0,0 +1,20 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  } else {
+    return a.age - b.age
+  }
+})
+
+console.log(students)
